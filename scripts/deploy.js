a7a8b98408f8092a413d83324b60dab66ab94e52
israelnojr <israeliloba@gmail.com>
2024-03-30 00:17:17 -0700

    first commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,43 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
+const {tokens, accessMetadata, performTransactions} = require("../helper-hardhat-config")
+const { metadata } = require('../metadata')
 
 async function main() {
+  const [buyer, seller, inspector, lender] = await ethers.getSigners()
+  const PeeLow = await ethers.getContractFactory("PeeLow")
+  const peeLow = await PeeLow.deploy()
+  await peeLow.deployed()
+  console.log(`Deployed Pelow Contract at: ${peeLow.address}`)
 
+  console.log(`Minting ${metadata.length} properties...\n`)
+
+  for (let i = 0; i < metadata.length; i++) {
+    const transaction = await peeLow.connect(seller).mint(`https://ipfs.io/ipfs/QmQVcpsjrA6cr1iJjZAodYwmPekYgbnXGo4DFubJiLc2EB/${i + 1}.json`)
+    await transaction.wait(1)
+  }
+
+  const Escrow = await ethers.getContractFactory('Escrow')
+  const escrow = await Escrow.deploy(
+    peeLow.address,
+    seller.address,
+    inspector.address,
+    lender.address
+  )
+  await escrow.deployed()
+
+  console.log(`Deployed Escrow Contract at: ${escrow.address}`)
+  console.log(`Listing ${metadata.length} properties...\n`)
+
+  for (let i = 0; i < metadata.length; i++) {
+    let transaction = await peeLow.connect(seller).approve(escrow.address, i + 1)
+    await transaction.wait(1)
+  }
+
+  performTransactions(escrow, buyer, seller).catch(err => console.error('Error performing transactions:', err));
+  console.log("Finished!")
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
